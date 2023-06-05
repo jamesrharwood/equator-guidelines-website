9879adb2e5e4f0131b1a19c5a9f0aaa62b2dae2e
James Harwood <jamesrharwood@gmail.com>
2023-06-05 17:10:28 +0100

    adds 404 and debug prints

--- build/create_resources.py
+++ build/create_resources.py
@@ -4,5 +4,6 @@ from .guideline import Guideline
 
 if __name__ == '__main__':
     dirname = sys.argv[1]
+    print("Creating guideline: "+dirname)
     gl = Guideline.create(dirname)
     gl.create_resources()
--- build/resources/create.py
+++ build/resources/create.py
@@ -1,4 +1,5 @@
 from .web.create import create as create_web
 
 def create_resources(guideline):
+    print("Creating web resource: "+guideline.dirname)
     create_web(guideline)
